d544f74adce58b6c9c504f12383f6121d74289e8
Cats <62059774+CatsKissat@users.noreply.github.com>
2022-02-08 11:15:19 +0200

    Update Damager.cs
    Updated Damager code.
    Removed damager interface

--- Damager.cs
+++ Damager.cs
@@ -1,25 +1,19 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Damager : MonoBehaviour, IDamager
+public class Damager : MonoBehaviour
 {
-	[Tooltip("How long prefabs stays active until it disables itself")] [SerializeField] private float timeToInactivate;
-	[Tooltip("Float value")] public FloatReference damageAmount;
-
-	public float damageValue
-	{
-		get => damageAmount;
-	}
+	[Tooltip("How long prefabs stays active until it disables itself")] [SerializeField] private float timeToInactivate = 0.1f;
+	// NOTE:
+	// This not actually include code to damage enemies.
+	// This is just for testing purpouse of a modular attacking.
 
 	private void OnEnable()
 	{
+		// Start Coroutine to inactivate object
+		// Great for bullets if you want them to inactivate after certain time
 		StartCoroutine(DisableAfterTime());
-	}
-
-	public void MakeDamage()
-	{
-		throw new System.NotImplementedException();
 	}
 
 	IEnumerator DisableAfterTime()
