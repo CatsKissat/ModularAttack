d830753de7371c529b47e7236032d1795d48535f
Cats <62059774+CatsKissat@users.noreply.github.com>
2022-02-08 10:59:17 +0200

    Add files via upload
    Uploaded a file that was accidentally deleted

---
+++ Damager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damager : MonoBehaviour, IDamager
+{
+	[Tooltip("How long prefabs stays active until it disables itself")] [SerializeField] private float timeToInactivate;
+	[Tooltip("Float value")] public FloatReference damageAmount;
+
+	public float damageValue
+	{
+		get => damageAmount;
+	}
+
+	private void OnEnable()
+	{
+		StartCoroutine(DisableAfterTime());
+	}
+
+	public void MakeDamage()
+	{
+		throw new System.NotImplementedException();
+	}
+
+	IEnumerator DisableAfterTime()
+	{
+		yield return new WaitForSeconds(timeToInactivate);
+		gameObject.SetActive(false);
+	}
+}
