d548b423ea88cbdd893dc123015b717baad6884a
Cats <62059774+CatsKissat@users.noreply.github.com>
2022-02-08 10:45:53 +0200

    Add files via upload
    Initial upload for the ModularAttack files

---
+++ AttackMeleeCommand.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackMeleeCommand : MonoBehaviour, IAttack
+{
+	[Tooltip("Location where an attack prefabs appears")] [SerializeField] private GameObject weaponHand;
+	[SerializeField] private GameObject attackPrefab;
+	[SerializeField] private int amountToPool = 5;
+	private List<GameObject> pooledPrefabs;
+	private GameObject attack;
+
+	private void Start()
+	{
+		CreatePoolOfAttacks();
+		attack = GetPooledAttack();
+	}
+
+	private void CreatePoolOfAttacks()
+	{
+		pooledPrefabs = new List<GameObject>();
+		GameObject temp;
+		for (int i = 0; i < amountToPool; i++)
+		{
+			temp = Instantiate(attackPrefab);
+			temp.SetActive(false);
+			pooledPrefabs.Add(temp);
+		}
+	}
+
+	public void Attack()
+	{
+		if(attack != null)
+		{
+			// Set pooled attack to weaponHand's location
+			attack.transform.position = weaponHand.transform.position;
+			attack.transform.rotation = weaponHand.transform.rotation;
+
+			attack.SetActive(true);
+		}
+	}
+
+	public GameObject GetPooledAttack()
+	{
+		for (int i = 0; i < amountToPool; i++)
+		{
+			if (!pooledPrefabs[i].activeInHierarchy)
+			{
+				return pooledPrefabs[i];
+			}
+		}
+		return null;
+	}
+}
---
+++ PlayerAttackBase.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerAttackBase : MonoBehaviour
+{
+	private IAttack attack;
+	private PlayerInputActions inputActions;
+
+	private void Awake()
+	{
+		inputActions = new PlayerInputActions();
+		inputActions.Gameplay.Attack.performed += OnAttack;
+
+		if (GetComponent<IAttack>() != null)
+		{
+			attack = GetComponent<IAttack>();
+		}
+		else
+		{
+			Debug.LogWarning(gameObject.name + " is missing Attack interface");
+		}
+	}
+
+	private void OnEnable()
+	{
+		inputActions.Gameplay.Attack.Enable();
+	}
+
+	private void OnAttack(InputAction.CallbackContext ctx)
+	{
+		if (attack != null)
+		{
+			attack.Attack();
+		}
+		else
+		{
+			Debug.LogWarning(gameObject.name + " is trying to attack but has no Attack interface");
+		}
+	}
+
+	private void OnDisable()
+	{
+		inputActions.Gameplay.Attack.Disable();
+	}
+}
